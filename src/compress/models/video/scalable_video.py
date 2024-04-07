c023de4ef448b311027be01ae72cc7548015d3b9
AlbertoPresta <43781044+AlbertoPresta@users.noreply.github.com>
2024-04-07 11:24:39 -0700

    continue_with_video

--- src/compress/models/video/scalable_video.py
+++ src/compress/models/video/scalable_video.py
@@ -93,10 +93,10 @@ class ScalableScaleSpaceFlow(ScaleSpaceFlow):
             mask_pol = self.mask_policy
         list_quality = self.define_quality(quality)  
         reconstructions_base = []
-        frames_likelihoods_base = []
+        frames_likelihoods = []
     
         reconstructions_prog = []
-        frames_likelihoods_prog = []
+
         
         frames_likelihoods_hype = []
 
@@ -108,11 +108,11 @@ class ScalableScaleSpaceFlow(ScaleSpaceFlow):
         
     
         reconstructions_base.append(x_hat[0])
-        frames_likelihoods_base.append(likelihoods["keyframe"][0])
+        frames_likelihoods.append(likelihoods["keyframe"])
 
 
         reconstructions_prog.append(x_hat[1])
-        frames_likelihoods_prog.append(likelihoods["keyframe"][1])
+
 
         frames_likelihoods_hype.append(likelihoods["keyhype"])
         x_ref_b = x_hat[0].detach()  # stop gradient flow (cf: google2020 paper)
@@ -128,25 +128,51 @@ class ScalableScaleSpaceFlow(ScaleSpaceFlow):
                                                     mask_pol =mask_pol, 
                                                     training =training)
             reconstructions_base.append(x_ref[0])
-            frames_likelihoods_base.append(likelihoods[0])
-
-
-
+            reconstructions_prog.append(x_ref[1])
+            frames_likelihoods.append(likelihoods)
+            frames_likelihoods_hype.append(likelihoods["hype"])
             
-
-            # fare la stessa cosa per la qualità 2
-            x = frames[i]
-            x_ref_prog, likelihoods = self.forward_inter(x, x_ref_prog, quality = 1, mask_pol =mask_pol, training =training)
-            reconstructions_base.append(x_ref)
-            frames_likelihoods_prog.append(likelihoods)           
-            
-
-            
+        reconstructions = [reconstructions_base,reconstructions_prog]
         return {
             "x_hat": reconstructions,
             "likelihoods": frames_likelihoods,
+            "hyperprior": frames_likelihoods_hype
         }
 
+
+
+    def forward_block(self,y,y_b,y_p,funct,training,mask_pol,quality):
+        z_motion = self.motion_hyperprior.hyper_encoder(y)
+        z_hat, z_likelihoods = funct.entropy_bottleneck(z_motion, 
+                                                        training = training)        
+        
+
+        scales = funct.hyper_decoder_scale(z_hat)
+        means = funct.hyper_decoder_mean(z_hat)    
+        
+        scales_b, scales_p = scales.chunk(2,1) # chunck scaes 
+        means_b,means_p = means.chuck(2,1) # chunk means 
+
+        # this is the base       
+        _, y_likelihoods_b = funct.gaussian_conditional(y_b, 
+                                                        scales_b, 
+                                                        means_b)
+        
+        y_hat_b  = quantize_ste(y_b - means_b) + means_b
+            
+
+        mask = funct.compute_mask(scales if self.double_dim else scales_p, 
+                                                mask_pol = mask_pol, 
+                                                quality = quality) 
+        
+        _, y_likelihoods_p = funct.gaussian_conditional((y_p - means_p)*mask,
+                                                        scales_p*mask)
+        
+        y_hat_p = quantize_ste(y_p - means_p)*mask  + means_p 
+        y_hat_p = y_hat_b + y_hat_p
+
+        return [y_hat_b,y_hat_p],[z_likelihoods,y_likelihoods_b,y_likelihoods_p]
+        
 
 
     def forward_keyframe(self, x, quality, mask_pol, training):  #sembra essere Balle18
@@ -157,31 +183,21 @@ class ScalableScaleSpaceFlow(ScaleSpaceFlow):
             y = torch.cat([y_b,y_p],dim = 1) 
         else: 
             y = self.img_encoder(x)
+            y_b, y_p =y.chunck(2,1) #chuck the two components
 
-        z = self.img_hyperprior.hyper_encoder(y)
-        z_hat, z_likelihoods = self.img_hyperprior.entropy_bottleneck(z, training = training) 
-        
-        scales = self.img_hyperprior.hyper_decoder_scale(z_hat)
-        means = self.img_hyperprior.hyper_decoder_mean(z_hat)    
-        
-        scales_b, scales_p = 0,0 # chunck scaes 
-        means_b,means_p = 0,0 # chunk means 
+        y_hat, likelihoods = self.forward_block(y,
+                                                y_b,
+                                                y_p,
+                                                self.img_hyperprior,
+                                                training,
+                                                mask_pol,
+                                                quality)
 
-        # this is the base       
-        _, y_likelihoods_b = self.img_hyperprior.gaussian_conditional(y_b, scales_b, means_b)
-        y_hat_b = quantize_ste(y - means_b) + means_b
+        y_hat_b,y_hat_p = y_hat[0],y_hat[1]
+        z_likelihoods = likelihoods[0]
+        y_likelihoods_b = likelihoods[1]
+        y_likelihoods_p = likelihoods[2]
         
-        
-        mask = self.img_hyperprior.compute_mask(scales if self.double_dim else scales_p, 
-                                                mask_pol = mask_pol, 
-                                                quality = quality) 
-        
-    
-        _, y_likelihoods_p = self.img_hyperprior.gaussian_conditional((y_p - means_p)*mask, scales_p*mask)
-        y_hat_p = quantize_ste(y_p - means_p)*mask  + means_p 
-        y_hat_p = y_hat_b + y_hat_p
-        
-    
         x_hat_b = self.img_decoder[0](y_hat_b) if self.multiple_ref_decoder else  self.img_decoder(y_hat_b)
         x_hat_p = self.img_decoder[1](y_hat_p) if self.multiple_ref_decoder else  self.img_decoder(y_hat_p)
         
@@ -191,42 +207,106 @@ class ScalableScaleSpaceFlow(ScaleSpaceFlow):
         return x_hat, {"keyhype": z_likelihoods,"keyframe": likelihoods}   
         
 
+
+
     def forward_inter(self, x_cur, x_ref, mask_pol, quality, training = True):
         # encode the motion information
         
 
-        x = torch.cat((x_cur, x_ref[0]), dim=1)  if quality == 0 else torch.cat((x_cur, x_ref[1]), dim=1)# cat the input 
+        x_b = torch.cat((x_cur, x_ref[0]), dim=1)  
+        x_p = torch.cat((x_cur, x_ref[1]), dim=1)# cat the input 
         
+        y_motion_b = self.motion_encoder[0](x_b) if self.multiple_flow_encoder else self.motion_encoder(x_b)
+        y_motion_p = self.motion_encoder[1](x_p) if self.multiple_flow_encoder else self.motion_encoder(x_p)
+        y_motion = torch.cat([y_motion_b,y_motion_p],dim = 1)
+
         
-        if self.multiple_flow_encoder:
-            y_motion_b = self.motion_encoder[0](x) 
-            y_motion_p = self.motion_encoder[1](x)
-            y_motion = torch.cat([y_motion_b,y_motion_p],dim = 1)
-        else: 
-            y_motion = self.motion_encoder(x)
+        y_hat_motion, likelihoods_motion = self.forward_block(y_motion,
+                                                            y_motion_b,
+                                                               y_motion_p,
+                                                             self.motion_hyperprior,
+                                                             training,
+                                                             mask_pol,
+                                                             quality)
+
+        y_hat_motion_b,y_hat_motion_p = y_hat_motion[0],y_hat_motion[1]
+        z_likelihoods_motion = likelihoods_motion[0]
+        y_likelihoods_motion_b = likelihoods_motion[1]
+        y_likelihoods_motion_p = likelihoods_motion[2]
         
-        z_motion = self.motion_hyperprior.hyper_encoder(y_motion)
-        z_hat_motion, z_likelihoods_motion = self.motion_hyperprior.entropy_bottleneck(z_motion, training = training)        
-        
+        motion_info_b = self.motion_decoder[0](y_hat_motion_b) if self.multiple_flow_decoder \
+                        else self.motion_decoder(y_hat_motion_b) # scale space flow Decoder 
+        x_pred_b = self.forward_prediction(x_ref[0], motion_info_b) # scale space warping
 
 
         # decode the space-scale flow information
-        motion_info = self.motion_decoder(y_motion_hat) # scale space flow Decoder 
-        x_pred = self.forward_prediction(x_ref, motion_info) # scale space warping
+        motion_info_p = self.motion_decoder[1](y_hat_motion_p) if self.multiple_flow_decoder \
+                        else self.motion_decoder(y_hat_motion_p) # scale space flow Decoder 
+        x_pred_p = self.forward_prediction(x_ref[1], motion_info_p)
 
-        # residual
-        x_res = x_cur - x_pred
-        y_res = self.res_encoder(x_res) # residual encoder, da aumentare 
-        y_res_hat, res_likelihoods = self.res_hyperprior(y_res) # y_res_hat = v_hat^i
+        # residual BASE
+        x_res_b = x_cur - x_pred_b
+        x_res_p = x_cur - x_pred_p
+        x_res = [x_res_b, x_res_p]
 
-        # y_combine
-        y_combine = torch.cat((y_res_hat, y_motion_hat), dim=1) #w_i + v_i
-        x_res_hat = self.res_decoder(y_combine) # x_res_hat = r^_i
+        y_res_b = self.res_encoder[0](x_res[0])  if self.multiple_res_encoder \
+                                                else self.res_encoder(x_res[0])# residual encoder, da aumentare 
+        
+        
+        y_res_p = self.res_encoder[1](x_res[1]) if self.multiple_res_encoder \
+                                                else self.res_encoder(x_res[1]) 
+
+        y_res = torch.cat([y_res_b,y_res_p],dim=1)
+
+        y_hat_res, likelihoods_res = self.forward_block(y_res,
+                                                            y_res_b,
+                                                               y_res_p,
+                                                             self.res_hyperprior,
+                                                             training,
+                                                             mask_pol,
+                                                             quality)
+
+        y_hat_res_b,y_hat_res_p = y_hat_res[0],y_hat_res[1]
+        z_likelihoods_res = likelihoods_res[0]
+        y_likelihoods_res_b = likelihoods_res[1]
+        y_likelihoods_res_p = likelihoods_res[2]
+
+
+        y_combine_b = torch.cat((y_hat_res_b,y_hat_motion_b), dim=1) #w_i + v_i
+        x_res_hat_b = self.res_decoder[0](y_combine_b) if self.multiple_res_decoder  \
+                                                    else self.res_decoder(y_combine_b) # x_res_hat = r^_i
+        # final reconstruction: prediction + residual
+        x_rec_b = x_pred_b + x_res_hat_b # final reconstruction
+
+
+
+
+        y_combine_p = torch.cat((y_hat_res_p,y_hat_motion_p), dim=1)
+        x_res_hat_p = self.res_decoder[1](y_combine_p) if self.multiple_res_decoder  \
+                                                    else self.res_decoder(y_combine_p) # x_res_hat = r^_i
 
         # final reconstruction: prediction + residual
-        x_rec = x_pred + x_res_hat # final reconstruction 
+        x_rec_p = x_pred_p + x_res_hat_p # final reconstruction
 
-        return x_rec, {"motion": motion_likelihoods, "residual": res_likelihoods}
+
+        hype_likelihoods = {
+            "motion": z_likelihoods_motion,
+            "res": z_likelihoods_res,
+        }
+        base_likelihoods = {
+            "motion": y_likelihoods_motion_b,
+            "residual":y_likelihoods_res_b
+        }
+
+        prog_likelihoods = {
+            "motion": y_likelihoods_motion_p,
+            "residual":y_likelihoods_res_p
+        }
+
+        x_rec  = [x_rec_b,x_rec_p]
+        return x_rec, {"base": base_likelihoods, 
+                        "prog": prog_likelihoods,
+                        "hype":hype_likelihoods}
 
 
         
