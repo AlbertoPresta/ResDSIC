187850f5b11ad72e681a3ec17877e8497098c88c
AlbertoPresta <43781044+AlbertoPresta@users.noreply.github.com>
2024-04-23 11:21:06 -0700

    correct bugs

--- src/compress/models/__init__.py
+++ src/compress/models/__init__.py
@@ -110,7 +110,7 @@ def get_model(args,device, lmbda_list):
                                 support_progressive_slices =args.support_progressive_slices,
                                 double_dim = args.double_dim,
                                 shared_entropy_estimation = False,
-                                delta_encoder = args.delta_encode,
+                                delta_encode = args.delta_encode,
                         )  
     elif args.model == "progressive_res":
         net = models[args.model]( N = args.N,
